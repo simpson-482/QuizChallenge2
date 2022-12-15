6645e2ee2a5f66ce742ad6e4737509d99674f0b7
Jared Simpson <66752595+simpson-482@users.noreply.github.com>
2022-12-15 16:27:59 -0500

    Quiz Challenge 2 copied

---
+++ questions.js
@@ -0,0 +1,126 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following was a Cleveland Browns runningback?",
+    answer: "Jim Brown",
+    options: [
+      "Walter Payton",
+      "Jim Brown",
+      "Larry Csonka",
+      "Herschel Walker"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which country has won the most World Cups?",
+    answer: "Brazil",
+    options: [
+      "Argentina",
+      "Germany",
+      "France",
+      "Brazil"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who was the 32nd president (1932-1945) of the United States?",
+    answer: "Franklin Roosevelt",
+    options: [
+      "Franklin Roosevelt",
+      "Ronald Reagan",
+      "Theodore Roosevelt",
+      "Thomas Jefferson"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the eleventh element in the periodic table?",
+    answer: "Sodium",
+    options: [
+      "Aluminum",
+      "Carbon",
+      "Sodium",
+      "Sulfur"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is an amino acid?",
+    answer: "Glutamine",
+    options: [
+      "Glutamine",
+      "Guanine",
+      "Purine",
+      "Neuron"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which is the volcanoe that produces lava in Hawaii Valcanoes National Park?",
+    answer: "Kilauea",
+    options: [
+        "Kilauea",
+        "Mount Vesuvius",
+        "Yellowstone",
+        "Mount Pelee"
+    ]
+},
+{
+    numb: 7,
+    question: "What is the largest lake in the world?",
+    answer: "Lake Superior",
+    options: [
+        "Lake Michigan",
+        "Lake Superior",
+        "Caspian Sea",
+        "Lake Baikal"
+    ]
+},
+{
+    numb: 8,
+    question: "Which of the following is a Dow Jones Company?",
+    answer: "Apple",
+    options: [
+        "American Airlines",
+        "Apple",
+        "Facebook",
+        "Netflix"
+    ]
+},
+{
+    numb: 9,
+    question: "What is the component that provides the basis of life for coral reefs?",
+    answer: "Calcium Carbonate",
+    options: [
+        "Calcium Carbonate",
+        "Nitrogen and Phosphorus",
+        "Salt Water and Cold Water",
+        "Fish and Algae"
+    ]
+},
+{
+    numb: 10,
+    question: "Who is the Greek God of the sun?",
+    answer: "Apollo",
+    options: [
+        "Zeus",
+        "Aphrodite",
+        "Hermes",
+        "Apollo"
+    ]
+},
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
